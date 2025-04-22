a348096d2ba2a2c8f8d67c1bedbec7ca542e4070
Navneet <nsh12727@gmail.com>
2025-04-22 17:41:24 +0530

    added sql server

--- config/db.js
+++ config/db.js
@@ -1,5 +1,4 @@
-require('dotenv').config(); 
-
+require('dotenv').config();
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
@@ -7,6 +6,10 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  port: 3306, 
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = pool;
