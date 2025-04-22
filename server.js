52902d0dd65a501dfdb17ea6ff2cb5195110b220
Navneet <nsh12727@gmail.com>
2025-04-22 12:11:56 +0530

    added testing and deployment

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const schoolRoutes = require('./routes/schoolRoutes');
@@ -7,6 +6,11 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 app.use('/', schoolRoutes); 
+
+app.get('/check', (req, res) => {
+  console.log("hii");
+  res.send("Check route is working!");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
